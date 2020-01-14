f83076665eecc187a05c6d701bfc816a8e3a1506
wglouie <louie@oakland.edu>
2020-01-13 23:09:37 -0500

    Commit changes to cpp_examples

--- ros/src/cpp_examples/main.cpp
+++ ros/src/cpp_examples/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 
 int main(int argc, char** argv){
+    
     printf("Argc value: %d", argc);
     if(argc != 3){
         printf("Incorrect number of arguements provided to this program. Three are required \n");
@@ -10,6 +11,12 @@ int main(int argc, char** argv){
         for(int i=0; i<argc; i++){
                 printf("%d: %s\n", i, argv[i]);
         }
+
+        int a = std::atoi(argv[1]);
+        int b = std::atoi(argv[2]);
+        int result = a + b;
+        
+        printf("%d + %d = %d\n", a, b, result); 
     }
     
     
